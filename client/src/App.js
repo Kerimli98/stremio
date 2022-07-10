e531ef85f4921dd7a591db2c9b33275e85452d91
Fatima Karimli <58202681+Kerimli98@users.noreply.github.com>
2022-07-10 21:53:21 +0400

    react-router

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,21 @@
-import './App.css';
+import { BrowserRouter, Route, Link } from "react-router-dom";
+import StreamCreate from "./components/streams/StreamCreate";
+import StreamDelete from "./components/streams/StreamDelete";
+import StreamEdit from "./components/streams/StreamEdit";
+import StreamList from "./components/streams/StreamList";
+import StreamShow from "./components/streams/StreamShow";
 
 function App() {
   return (
-    <div className="App">
-  
+    <div>
+      <h1>Header</h1>
+      <BrowserRouter>
+        <Route path="/" exact component={StreamList} />
+        <Route path="/streams/new" component={StreamCreate} />
+        <Route path="/streams/edit" component={StreamEdit} />
+        <Route path="/streams/delete" component={StreamDelete} />
+        <Route path="/streams/show" component={StreamShow} />
+      </BrowserRouter>
     </div>
   );
 }
