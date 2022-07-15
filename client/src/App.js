0628c4f0916eda6154590cedbd0bb95f670744c3
Fatima Karimli <58202681+Kerimli98@users.noreply.github.com>
2022-07-15 12:21:33 +0400

    finished rest apis

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,22 @@
-import { BrowserRouter, Route } from "react-router-dom";
+import { Router, Route } from "react-router-dom";
 import Header from "./components/Header";
 import StreamCreate from "./components/streams/StreamCreate";
 import StreamDelete from "./components/streams/StreamDelete";
 import StreamEdit from "./components/streams/StreamEdit";
 import StreamList from "./components/streams/StreamList";
 import StreamShow from "./components/streams/StreamShow";
-
+import history from "./history";
 function App() {
   return (
     <div className="ui container">
-      <BrowserRouter>
-      <Header />
-
+      <Router history={history}>
+        <Header />
         <Route path="/" exact component={StreamList} />
         <Route path="/streams/new" component={StreamCreate} />
-        <Route path="/streams/edit" component={StreamEdit} />
+        <Route path="/streams/edit/:id" component={StreamEdit} />
         <Route path="/streams/delete" component={StreamDelete} />
         <Route path="/streams/show" component={StreamShow} />
-      </BrowserRouter>
+      </Router>
     </div>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { Provider } from "react-redux";
-import { createStore , compose, applyMiddleware} from "redux";
-import reducers from './reducers'
+import { createStore, compose, applyMiddleware } from "redux";
+import reduxThunk from "redux-thunk";
+import reducers from "./reducers";
 const root = ReactDOM.createRoot(document.getElementById("root"));
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
-const store=createStore(reducers, composeEnhancers(applyMiddleware()));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(applyMiddleware(reduxThunk)));
 root.render(
   <Provider store={store}>
     <App />
